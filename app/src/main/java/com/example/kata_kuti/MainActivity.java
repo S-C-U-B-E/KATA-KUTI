cd90e4df3f8853474fe785afb75f172151563605
S-C-U-B-E <sssanyal10@gmail.com>
2020-04-05 13:07:07 +0530

    1.2.3 Game Rounds fixed... Working on Score Update..

--- app/src/main/java/com/example/kata_kuti/MainActivity.java
+++ app/src/main/java/com/example/kata_kuti/MainActivity.java
@@ -44,18 +44,25 @@ public class MainActivity extends AppCompatActivity {
      static ArrayList<List<Integer>> winningSet;
 
     //Stores the choice of the number of rounds the players wanted to play
+    //Strores the current Round Number
      private static int mRound = 1;
+     private static int mCurrentRound ;
 
     //choice List
      private Spinner mChoiceList;
 
-    //Start Button
+    //Start Button , Next Round Button
      private Button mButtonStart;
+     private Button mButtonNext;
 
     //To display the current Onging round
      private String mGameRoundMessage;
 
-     TextView gameResultMessageBox,gameRoundMessageBox;
+    //To store scores of players in a match.
+     private int mScorePlayer1;
+     private int mScorePlayer2;
+
+     TextView gameResultMessageBox,gameRoundMessageBox,gameScorePlayer1,gameScorePlayer2;
      ImageView cell00,cell01,cell02,cell10,cell11,cell12,cell20,cell21,cell22;
 
 
@@ -71,17 +78,23 @@ public class MainActivity extends AppCompatActivity {
         textViewPlayer2.setBackgroundColor(Color.parseColor("#757575"));// SET TO INITIAL COLOR DENOTING 1's TURN AT THE BEGINNING OF GAME
 
 
+        mCurrentRound = 0;
         cellMap = new SparseIntArray();
         winningSet = new ArrayList<>();
+
         gameResultMessageBox = findViewById(R.id.textviewgameresultmessage);
         gameRoundMessageBox = findViewById(R.id.textviewgameroundmessage);
-        mButtonStart = findViewById(R.id.buttonstart);
+        gameScorePlayer1 = findViewById(R.id.textviewscore1);
+        gameScorePlayer2 = findViewById(R.id.textviewscore2);
 
+        mButtonStart = findViewById(R.id.buttonstart);
+        mButtonNext = findViewById(R.id.buttonnext);
 
                 /*
         * Initially (Before selection of mRound and start of game) Gamee Round text box is disabeled.
         * */
         gameRoundMessageBox.setVisibility(View.GONE);
+        mButtonNext.setVisibility(View.GONE);
 
         mChoiceList = (Spinner) findViewById(R.id.rounds);
         setupSpinner();
@@ -103,14 +116,18 @@ public class MainActivity extends AppCompatActivity {
          setupCellMap();
 
         /*
-         * Click Listeners set for the Cells
+         * Click on Start Button to start the game XD
          * */
-        /*if(roundValueSelected){
-            setupOnClickListeners();
-        }*/
-
-        //setupOnClickListeners();
         mButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+                Toast.makeText(MainActivity.this,"Out of startGame()",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mButtonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startGame();
@@ -162,6 +179,42 @@ public class MainActivity extends AppCompatActivity {
         cellMap.put(R.id.cell20,7);
         cellMap.put(R.id.cell21,8);
         cellMap.put(R.id.cell22,9);
+    }
+
+    private void setupSpinner(){
+        // Created an ArrayAdapter using the string array and a default mChoiceList layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.rounds_array, android.R.layout.simple_spinner_item);
+        // the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // the adapter to the mChoiceList XD
+        mChoiceList.setAdapter(adapter);
+
+
+        // Set the integer mSelected to the constant values
+        mChoiceList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    if (selection.equals("1")) {
+                        mRound = 1;
+                    } else if (selection.equals("2")) {
+                        mRound = 2;
+                    } else if (selection.equals("3")) {
+                        mRound = 3;
+                    } else {
+                        mRound = 4;
+                    }
+                }
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                mRound = 1;
+            }
+        });
     }
 
     private void setupOnClickListeners(){
@@ -234,42 +287,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setupSpinner(){
-        // Created an ArrayAdapter using the string array and a default mChoiceList layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.rounds_array, android.R.layout.simple_spinner_item);
-        // the layout to use when the list of choices appears
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // the adapter to the mChoiceList XD
-                mChoiceList.setAdapter(adapter);
-
-
-        // Set the integer mSelected to the constant values
-        mChoiceList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selection = (String) parent.getItemAtPosition(position);
-                if (!TextUtils.isEmpty(selection)) {
-                    if (selection.equals("1")) {
-                        mRound = 1;
-                    } else if (selection.equals("2")) {
-                        mRound = 2;
-                    } else if (selection.equals("3")) {
-                        mRound = 3;
-                    } else {
-                        mRound = 4;
-                    }
-                }
-            }
-
-            // Because AdapterView is an abstract class, onNothingSelected must be defined
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                mRound = 1;
-            }
-        });
-    }
-
     private void gamePlay(View view){
 
         ImageView imageView = findViewById(view.getId());
@@ -297,10 +314,13 @@ public class MainActivity extends AppCompatActivity {
             if(clickCount > 3)foundWinner = theWinner(clickCount);
             if(foundWinner){
                 if(clickCount%2 == 0){
+                   // mScorePlayer2++;
                     gameResultMessageBox.setText("PLAYER 2 WON");textViewPlayer2.setBackgroundColor(Color.parseColor("#757575"));textViewPlayer1.setBackgroundColor(Color.parseColor("#757575"));}
                 else {
+                    //mScorePlayer1++;
                     gameResultMessageBox.setText("PLAYER 1 WON");textViewPlayer2.setBackgroundColor(Color.parseColor("#757575"));textViewPlayer1.setBackgroundColor(Color.parseColor("#757575"));}
 
+               if(mCurrentRound<mRound)mButtonNext.setVisibility(View.VISIBLE);
             }else if(clickCount == 9){
                 /*All cells have been set but winner is still not found
                  *Logic for handling Tie
@@ -308,37 +328,73 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(MainActivity.this,"It's a Tie",Toast.LENGTH_SHORT).show();
                 gameResultMessageBox.setText("IT'S A TIE");
                 textViewPlayer2.setBackgroundColor(Color.parseColor("#757575"));textViewPlayer1.setBackgroundColor(Color.parseColor("#757575"));
+                if(mCurrentRound<mRound)mButtonNext.setVisibility(View.VISIBLE);
+               // mScorePlayer2++;mScorePlayer1++;
             }
 
         }
 
+        /*gameScorePlayer2.setText(mScorePlayer2);
+        gameScorePlayer1.setText(mScorePlayer1);*/
     }
 
     private void startGame(){
+        refreshTheCellsForNextRound();
+
         TextView textViewGameRoundMessage;
         TextView textViewPlayer1 = findViewById(R.id.textviewplayer1);
         textViewPlayer1.setBackgroundColor(Color.parseColor("#424242"));// SET TO INITIAL COLOR DENOTING 1's TURN AT THE BEGINNING OF GAME
         textViewGameRoundMessage = findViewById(R.id.textviewgameroundmessage);
 
-        clickCount = 0;
-        foundWinner = false;
-        listOfCellsAlreadySet = new ArrayList<>();
-        listOfCellsAlreadySet.add(-1);
-        Player1 = new ArrayList<>();
-        Player2 = new ArrayList<>();
-
         mChoiceList.setEnabled(false);
-
-        createGameRoundMessage();
 
         gameRoundMessageBox.setVisibility(View.VISIBLE);
         mButtonStart.setVisibility(View.GONE);
+        mButtonNext.setVisibility(View.GONE);
 
-        textViewGameRoundMessage.setText(mGameRoundMessage);
         setupOnClickListeners();
+
+        Toast.makeText(MainActivity.this,"mRound: "+mRound,Toast.LENGTH_SHORT).show();
+
+            mCurrentRound++;Toast.makeText(MainActivity.this,"mCurrentRound: "+mCurrentRound,Toast.LENGTH_SHORT).show();
+
+            mScorePlayer1 = 0;
+            mScorePlayer2 = 0;
+            clickCount = 0;
+            foundWinner = false;
+            listOfCellsAlreadySet = new ArrayList<>();
+            listOfCellsAlreadySet.add(-1);
+            Player1 = new ArrayList<>();
+            Player2 = new ArrayList<>();
+            gameResultMessageBox.setText("");
+
+            createGameRoundMessage(mCurrentRound);
+            textViewGameRoundMessage.setText(mGameRoundMessage);
+
+            updateScores();
     }
 
-    private void createGameRoundMessage(){
-        mGameRoundMessage = "Round - 1/"+mRound;
+    private void createGameRoundMessage(int mCurrentRound){
+        mGameRoundMessage = "Round - "+mCurrentRound+"/"+mRound;
+    }
+
+    private void updateScores(){
+
+    }
+
+    private void refreshTheCellsForNextRound(){
+        cell00.setImageResource(0);
+        cell01.setImageResource(0);
+        cell02.setImageResource(0);
+
+
+        cell10.setImageResource(0);
+        cell11.setImageResource(0);
+        cell12.setImageResource(0);
+
+
+        cell20.setImageResource(0);
+        cell21.setImageResource(0);
+        cell22.setImageResource(0);
     }
 }
